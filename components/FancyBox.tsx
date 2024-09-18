cefef4e13a371ca5ab2ce1a07f5929514f16d125
Michel Kowalski <mi89ke@hotmail.com>
2024-09-18 10:25:14 +0200

    first commit

---
+++ components/FancyBox.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function FancyBox() {
+  return (
+    <View style={{ backgroundColor: "green", width: 50, height: 60 }}>
+      <Text>Fancy</Text>
+    </View>
+  );
+}
